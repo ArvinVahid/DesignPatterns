c844a895d9311422f62602c468339ff67f6304cf
Arvin <arvinvahidkia1@gmail.com>
2024-09-24 09:35:27 +0330

    Bridge Pattern Added

---
+++ BridgePattern/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace BridgePattern
+{
+    internal class Program
+    {
+        public interface IMessageSender
+        {
+            void SendMessage(string content);
+        }
+
+        public abstract class Message
+        {
+            protected IMessageSender _messageSender;
+            public Message(IMessageSender messageSender)
+            {
+                _messageSender = messageSender;
+            }
+
+            public abstract void Send(string content);
+        }
+
+        public class EmailSender : IMessageSender
+        {
+            public void SendMessage(string content)
+            {
+                Console.WriteLine($"Sending Email with content {content}");
+            }
+        }
+
+        public class SmsSender : IMessageSender
+        {
+            public void SendMessage(string content)
+            {
+                Console.WriteLine($"Sending SMS with content {content}");
+            }
+        }
+
+        public class ShortMessage : Message
+        {
+            public ShortMessage(IMessageSender messageSender) : base(messageSender)
+            {
+            }
+
+            public override void Send(string content)
+            {
+                if (content.Length <= 10)
+                {
+                    // Console.WriteLine($"Sending short SMS with content {content}")
+                    _messageSender.SendMessage(content);
+                }
+                else
+                {
+                    Console.WriteLine("Content too long for a short message");
+                }
+            }
+        }
+
+        public class LongMessage : Message
+        {
+            public LongMessage(IMessageSender messageSender) : base(messageSender)
+            {
+                
+            }
+
+            public override void Send(string content)
+            {
+                _messageSender.SendMessage(content);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IMessageSender emailSender = new EmailSender();
+            IMessageSender smsSender = new SmsSender();
+
+            Message shortEmail = new ShortMessage(emailSender);
+            Message longSms = new LongMessage(smsSender);
+
+            shortEmail.Send("Hello");
+            longSms.Send("This is a longer message sent via SMS");
+        }
+    }
+
+    
+}
---
+++ BridgePattern/Standard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BridgePattern.Program;
+
+namespace BridgePattern
+{
+    internal class Standard
+    {
+        public abstract class Message
+        {
+            public abstract void Send(string content);
+        }
+
+        public class ShortEmailMessage : Message
+        {
+            public override void Send(string content)
+            {
+                if (content.Length <= 10)
+                {
+                    Console.WriteLine($"Sending short EMAIL with content {content}");
+                }
+                else
+                {
+                    Console.WriteLine("Content too long for a short Email");
+                }
+            }
+        }
+
+        public class LongEmailMessage : Message
+        {
+            public override void Send(string content)
+            {
+                Console.WriteLine($"Sending Long EMAIL with content {content}");
+            }
+        }
+
+        public class ShortSmsMessage : Message
+        {
+            public override void Send(string content)
+            {
+                if (content.Length <= 10)
+                {
+                    Console.WriteLine($"Sending short SMS with content {content}");
+                }
+                else
+                {
+                    Console.WriteLine("Content too long for a short message");
+                }
+            }
+        }
+
+        public class LongSmsMessage : Message
+        {
+            public override void Send(string content)
+            {
+                Console.WriteLine($"Sending Long SMS with content {content}");
+            }
+        }
+    }
+}
